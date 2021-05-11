a0df83a7e29d469f5d3e0e042734eb9c35f9cc4c
MaryamNona <>
2021-05-11 23:22:02 +0000

    Added basic math

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ICS2O-Unit2-02-Go-Basic_Math
+
+[![GitHub's Super Linter](https://github.com/MaryamNona/ICS2O-Unit2-02-Go-Basic_Math/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MaryamNona/ICS2O-Unit2-02-Go-Basic_Math/actions)
---
+++ basicMath.go
@@ -0,0 +1,17 @@
+// Created by: Maryam Nona
+// Created on: May 2021
+//
+// This program does basic math
+
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  //this function does basic math
+  fmt.Println("The dimensions of a rectangle are: 5 cm and 3 cm")
+  fmt.Println("The area is:", (5 * 3))
+  fmt.Println("The perimeter is:", (5 + 3) * 2)
+}
