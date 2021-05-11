e422ca0b2c9e4c50fb03ce8ebcb831e807888bdd
MaryamNona <>
2021-05-11 23:38:20 +0000

    fixed error

--- basicMath.go
+++ basicMath.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-  //this function does basic math
+ // this function does basic math
   fmt.Println("The dimensions of a rectangle are: 5 cm and 3 cm")
   fmt.Println("The area is:", (5 * 3))
   fmt.Println("The perimeter is:", (5 + 3) * 2)
